25ad9ec855707a69ddfd5977caa97d8ee7243bfa
Carlos Martinez <cmartinez@redvelgames.com>
2024-05-24 18:05:08 +0200

    feat: create expense tracker api controller

---
+++ ExpenseTracker.Presentation/ExpenseTrackerController.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTracker.Application;
+using ExpenseTracker.Application.Ports;
+using ExpenseTracker.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Presentation;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpenseTrackerController(ExpensesRepository repository) : ControllerBase
+{
+    readonly Tracker tracker = new(repository);
+
+    [HttpGet(Name = "RetrieveAllExpenses")]
+    public ActionResult<IEnumerable<Expense>> RetrieveAllExpenses()
+    {
+        return Ok(tracker.RetrieveAllExpenses());
+    }
+
+    [HttpPost(Name = "StoreExpense")]
+    public ActionResult<Expense> StoreExpense(Expense expense)
+    {
+        tracker.Store(expense);
+
+        return CreatedAtAction(nameof(StoreExpense), expense);
+    }
+}
