2c5ae19e28742db8adeab5323b87a77b576ace71
Carlos Martinez <cmartinez@redvelgames.com>
2024-05-25 11:52:22 +0200

    feat: know when a transaction has been made

--- ExpenseTracker.Domain/Transaction.cs
+++ ExpenseTracker.Domain/Transaction.cs
@@ -3,9 +3,16 @@ namespace ExpenseTracker.Domain;
 public record Transaction
 {
     public int Amount { get; }
+    public DateTime When { get; }
 
     public Transaction(int amount)
     {
         Amount = amount;
+    }
+    
+    public Transaction(int amount, DateTime when) 
+        : this(amount)
+    {
+        When = when;
     }
 }
