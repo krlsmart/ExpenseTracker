b64edf26217dd4b6454a14ad6d019e4dee5724ad
Carlos Martinez <krlsmartnz@gmail.com>
2026-02-07 11:41:20 +0100

    chore: move account tests to tests folder

---
+++ tests/ExpenseTracker.Application.Tests/AccountShould.cs
@@ -0,0 +1,55 @@
+﻿using ExpenseTracker.Application.Ports;
+using ExpenseTracker.Domain;
+using ExpenseTracker.Infrastructure;
+using NSubstitute;
+
+namespace ExpenseTracker.Application.Tests;
+
+public class AccountShould
+{
+    [Fact]
+    public async Task RetrieveAllExpenses()
+    {
+        var repositoryMock = Substitute.For<TransactionsRepository>();
+        var sut = BuilderFor.Account(repositoryMock);
+        
+        await sut.RetrieveAllTransactions();
+        
+        await repositoryMock.Received(1).RetrieveAll();
+    }
+    
+    //TODO: Este test es bastante inutil. Borrar en cuanto se añada la bbdd real
+    [Fact]
+    public async Task RetrieveAllExpenses_FromInMemoryRepository()
+    {
+        var sut = BuilderFor.Account(new InMemoryTransactionsRepository());
+
+        await sut.CreateAndStoreExpense(100);
+        await sut.CreateAndStoreExpense(500);
+        var result = await sut.RetrieveAllTransactions();
+
+        Assert.Equal(2, result.Count());
+    }
+    
+    [Fact]
+    public async Task AddExpense()
+    {
+        var repositoryMock = Substitute.For<TransactionsRepository>();
+        var sut = BuilderFor.Account(repositoryMock);
+
+        await sut.CreateAndStoreExpense(100);
+
+        await repositoryMock.Received(1).Store(Arg.Is<Transaction>(t => t.Amount == -100));
+    }
+    
+    [Fact]
+    public async Task AddIncome()
+    {
+        var repositoryMock = Substitute.For<TransactionsRepository>();
+        var sut = BuilderFor.Account(repositoryMock);
+
+        await sut.CreateAndStoreIncome(250);
+
+        await repositoryMock.Received(1).Store(Arg.Is<Transaction>(t => t.Amount == 250));
+    }
+}
---
+++ tests/ExpenseTracker.Application.Tests/BuilderFor.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Application.Ports;
+using NSubstitute;
+
+namespace ExpenseTracker.Application.Tests;
+
+public static class BuilderFor
+{
+    public static Account Account(TransactionsRepository? repositoryMock = null, Clock? clockMock = null)
+    {
+        var repository = repositoryMock ?? Substitute.For<TransactionsRepository>();
+        var clock = clockMock ?? Substitute.For<Clock>();
+        return new Account(repository, clock);
+    }
+}
